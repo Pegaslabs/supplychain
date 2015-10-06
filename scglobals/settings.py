c462083b494adf01bee45be6d727012bbf5d2407
Kevin Doran <kev.doran@gmail.com>
2015-10-06 03:57:44 -0500

    transaction view moved to shipment view, testing.py to remove corrupt shipment

--- scglobals/settings.py
+++ scglobals/settings.py
@@ -15,8 +15,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/kdoran/data/bots_sqlite_db',
-        # 'NAME': '/opt/botsenv/data/bots_sqlite_db',
+        # 'NAME': '/Users/kdoran/data/bots_sqlite_db',
+        'NAME': '/opt/botsenv/data/bots_sqlite_db',
         # 'NAME': "supplychain",                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         # 'USER': 'botsims',
